d67d1842695784bdfa44fe942f8e17e8f61dea56
ckarumoreira <ckarumoreira@outlook.com>
2018-04-14 00:12:09 -0300

    minor fixes

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -2,4 +2,4 @@
    angular.module('ng-loading-wrapper', [ 'ng-loading-wrapper.directives', 'ng-loading-wrapper.services' ]);
    angular.module('ng-loading-wrapper.directives', []);
    angular.module('ng-loading-wrapper.services', []);
-});
+})();
