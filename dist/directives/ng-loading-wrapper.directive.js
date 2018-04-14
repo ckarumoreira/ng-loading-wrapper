5d416e6392ac5e4cb04ad20256e7beb81504b37c
ckarumoreira <ckarumoreira@outlook.com>
2018-04-14 00:28:00 -0300

    loading mode changed

--- dist/directives/ng-loading-wrapper.directive.js
+++ dist/directives/ng-loading-wrapper.directive.js
@@ -1,6 +1,27 @@
 (function () {
     angular.module('ng-loading-wrapper.directives').directive('ngLoadingWrapper', {
-        templateUrl: 'app/directives/ng-loading-wrapper.template.html',
-        restrict: 'C'
+        restrict: 'C',
+        link: function (scope, element, attrs, controller, transclude) {
+            console.log("ngLoadingWrapper appended loading element.");
+
+            var target = angular.element(element);
+
+            var pane = angular.element('<div class="loading-pane"></div>');
+            var object = angular.element('<div class="loading-object"></div>');
+            var spinner = angular.element('<div class="loading-spinner"></div>');
+            var text = angular.element('<div class="loading-text">' + attrs.loadingText + '</div>');
+
+            object
+                .append(spinner)
+                .append(text);
+            pane.append(object);
+            
+            target.append(target);
+
+            scope.$on('$destroy', function () {
+                pane.remove();
+                console.log("ngLoadingWrapper remove loading element.");
+            });
+        }
     });
 })();
